e54e7ac3502f07718c7ca8322b95e95bca07579e
techpool <suryadeep10@gmail.com>
2017-08-24 18:21:37 +0530

    Updating PWA files

--- src/pwa-service-worker/pwa-sw-PROD_BENGALI.js
+++ src/pwa-service-worker/pwa-sw-PROD_BENGALI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?240820171816',
+		'pwa-stylesheets/css/style.css?240820171821',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -27,7 +27,7 @@
 		'https://www.ptlp.co/resource-all/font/font-bn.css'
 	];
 
-	var STATIC_VERSION = "240820171816";
+	var STATIC_VERSION = "240820171821";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -36,7 +36,7 @@
 	var apiPrefix = "https://bengali-prod.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-240820171816.html";
+	var PWA_INDEX_HTML = "app-shell-240820171821.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
