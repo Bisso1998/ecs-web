d2694b54fe9f9ac7b04745937a408f4983098201
genirahul <genirahul@gmail.com>
2017-09-09 20:08:12 +0530

    Updating PWA files

--- src/pwa-service-worker/pwa-sw-DEVO_ALL_LANGUAGE_GR.js
+++ src/pwa-service-worker/pwa-sw-DEVO_ALL_LANGUAGE_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?090920171940',
+		'pwa-stylesheets/css/style.css?090920172008',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -27,7 +27,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "090920171940";
+	var STATIC_VERSION = "090920172008";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -36,7 +36,7 @@
 	var apiPrefix = "https://www-devo-gr.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-090920171940.html";
+	var PWA_INDEX_HTML = "app-shell-090920172008.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
