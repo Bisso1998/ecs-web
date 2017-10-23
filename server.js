ee28a6fa4da3c03600c5fdfc3698e6c96e6778e2
techpool <suryadeep10@gmail.com>
2017-10-23 15:47:51 +0530

    Updating PWA files

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var httpsAgent = new https.Agent({ keepAlive : true });
 // Constants
 const PORT = 80;
 process.env.UV_THREADPOOL_SIZE = 128;
+const PRODUCT_PERCENTAGE = 50;
 
 function Enum() {
 	this._enums = [];
@@ -57,6 +58,10 @@ Enum.prototype = {
 	toString: function() {
 		return this.__name__;
 	}
+};
+
+String.prototype.contains = function (str, startIndex) {
+    return -1 !== String.prototype.indexOf.call(this, str, startIndex);
 };
 
 function defineEnum( definition ) {
@@ -181,34 +186,56 @@ app.get( '/health', (req, res, next) => {
 app.get( '/*', (req, res, next) => {
 
 	var website = _getWebsite( req.headers.host );
+	var bucketId = req.headers["bucket-id"];
+	var totalGrowthBuckets = req.headers["total-growth-buckets"] || 10;
+	var variation = 'build/growth/';
+
+	if (bucketId) {
+		const numberOfBucketsToShowProduct = Math.floor((PRODUCT_PERCENTAGE / 100) * totalGrowthBuckets);
+		if (Number(bucketId) <= numberOfBucketsToShowProduct) {
+			variation = 'build/product/';
+		}
+	}
+
+	if (req.query.variation === 'GROWTH') {
+		variation = 'build/growth/';
+	} else if (req.query.variation === 'PRODUCT') {
+		variation = 'build/product/';
+	}
+
+	if (req.header('Referer') && req.header('Referer').contains('variation=GROWTH')) {
+		variation = 'build/growth/';
+	} else if (req.header('Referer') && req.header('Referer').contains('variation=PRODUCT')) {
+		variation = 'build/product/';
+	}
 
 	if( req.path === '/pwa-stylesheets/css/style.css' ) {
-		fs.readFile( 'src/pwa-stylesheets/style.css', { 'encoding': 'utf8' }, (err, data) => {
+		fs.readFile( variation + 'src/pwa-stylesheets/style.css', { 'encoding': 'utf8' }, (err, data) => {
 			if(err) throw err;
 			res.set( 'Content-Type', 'text/css' ).send(data);
 		});
 	} else if( req.path === '/pwa-sw-' + website.__name__ + '.js' ) {
-		fs.readFile( 'src/pwa-service-worker' + req.path, { 'encoding': 'utf8' }, (err, data) => {
+		fs.readFile( variation + 'src/pwa-service-worker' + req.path, { 'encoding': 'utf8' }, (err, data) => {
 			if(err) throw err;
 			res.set( 'Content-Type', 'text/javascript' ).send(data);
 		});
 	} else if( req.path === '/favicon.ico' || req.path === '/favicon.png' ) {
-		res.sendfile( 'src/favicon.ico' );
+		res.sendfile( variation + 'src/favicon.ico' );
 	} else if( req.path.indexOf( '/pwa-images/' ) === 0 ) {
-		res.sendfile( 'src' + req.path );
+		res.sendfile( variation + 'src' + req.path );
 	} else if( req.path.indexOf( '/resources/' ) === 0 || req.path.indexOf( '/stylesheets/' ) === 0 ) {
 		res.set( 'Content-Type', 'text/plain' ).send( "" );
 	} else if( req.path === "/pwa-manifest-" + website.__name__ + ".json" ) {
-		fs.readFile( 'src/pwa-manifest' + '/pwa-manifest-' + website.__name__ + '.json', { 'encoding': 'utf8' }, (err, data) => {
+		fs.readFile( variation + 'src/pwa-manifest' + '/pwa-manifest-' + website.__name__ + '.json', { 'encoding': 'utf8' }, (err, data) => {
 			if(err) throw err;
 			res.set( 'Content-Type', 'application/json' ).send(data);
 		});
 	} else if( req.path === '/pratilipi-logo-144px.png' ) {
-		res.sendfile( 'src' + req.path );
+		res.sendfile( variation + 'src' + req.path );
 	} else {
 		// https://github.com/expressjs/express/issues/3127
 		console.log( "Serving html file to url :: ",  req.url );
-		fs.readFile( 'src/pwa-markup/PWA-' + website.__name__ + '.html', { 'encoding': 'utf8' }, (err, data) => {
+		fs.readFile( variation + 'src/pwa-markup/PWA-' + website.__name__ + '.html', { 'encoding': 'utf8' }, (err, data) => {
 			if(err) throw err;
 			res.set( 'Content-Type', 'text/html' ).send(data);
 		});
