65b0d6c26ab2d8294d3ecf1ea99c341dc240e948
techpool <suryadeep10@gmail.com>
2017-11-26 20:11:15 +0530

    Updating PWA files

--- build/glossy-cover/src/pwa-service-worker/pwa-sw-MALAYALAM.js
+++ build/glossy-cover/src/pwa-service-worker/pwa-sw-MALAYALAM.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?261120171957',
+		'pwa-stylesheets/css/style.css?261120172011',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ml.css'
 	];
 
-	var STATIC_VERSION = "261120171957";
+	var STATIC_VERSION = "261120172011";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://malayalam.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-261120171957.html";
+	var PWA_INDEX_HTML = "app-shell-261120172011.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
