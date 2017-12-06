37b73cec6e19bc8ce85c1bd5e654be5075a6eaba
techpool <suryadeep10@gmail.com>
2017-12-06 12:57:55 +0530

    Updating PWA files

--- build/glossy-cover/src/pwa-service-worker/pwa-sw-HINDI.js
+++ build/glossy-cover/src/pwa-service-worker/pwa-sw-HINDI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?051220171848',
+		'pwa-stylesheets/css/style.css?061220171257',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-hi.css'
 	];
 
-	var STATIC_VERSION = "051220171848";
+	var STATIC_VERSION = "061220171257";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://hindi.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-051220171848.html";
+	var PWA_INDEX_HTML = "app-shell-061220171257.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
