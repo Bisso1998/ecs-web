cadb64d0ba34de3c2a4f147fd638fa9c216e2bf1
Suryadeep Pal <suryadeep10@gmail.com>
2017-12-19 09:20:35 +0530

    Updating PWA files

--- build/cover-modification/src/pwa-service-worker/pwa-sw-DEVO_GUJARATI_GR.js
+++ build/cover-modification/src/pwa-service-worker/pwa-sw-DEVO_GUJARATI_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?191220170910',
+		'pwa-stylesheets/css/style.css?191220170920',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-gu.css'
 	];
 
-	var STATIC_VERSION = "191220170910";
+	var STATIC_VERSION = "191220170920";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://gujarati-devo-gr.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-191220170910.html";
+	var PWA_INDEX_HTML = "app-shell-191220170920.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
