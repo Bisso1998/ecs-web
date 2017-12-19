f3d41abc87f88508d4be6fdccfc17d9b229c439c
Suryadeep Pal <suryadeep10@gmail.com>
2017-12-19 16:24:56 +0530

    Updating PWA files

--- build/cover-modification/src/pwa-service-worker/pwa-sw-DEVO_ALL_LANGUAGE.js
+++ build/cover-modification/src/pwa-service-worker/pwa-sw-DEVO_ALL_LANGUAGE.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?181220171545',
+		'pwa-stylesheets/css/style.css?191220171624',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "181220171545";
+	var STATIC_VERSION = "191220171624";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www-devo.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-181220171545.html";
+	var PWA_INDEX_HTML = "app-shell-191220171624.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
