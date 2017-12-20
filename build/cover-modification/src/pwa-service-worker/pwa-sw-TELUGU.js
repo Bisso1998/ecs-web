2110fe131f44bb75ae570aa0286ad960317c9fdc
Suryadeep Pal <suryadeep10@gmail.com>
2017-12-20 15:11:06 +0530

    Updating PWA files

--- build/cover-modification/src/pwa-service-worker/pwa-sw-TELUGU.js
+++ build/cover-modification/src/pwa-service-worker/pwa-sw-TELUGU.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?201220171318',
+		'pwa-stylesheets/css/style.css?201220171510',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-te.css'
 	];
 
-	var STATIC_VERSION = "201220171318";
+	var STATIC_VERSION = "201220171510";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://telugu.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-201220171318.html";
+	var PWA_INDEX_HTML = "app-shell-201220171510.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
