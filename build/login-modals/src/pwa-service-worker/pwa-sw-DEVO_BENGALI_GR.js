7de9d7b6f8ef8367751beb1b5a590b3659337028
Libin V Babu <libinvbabu@gmail.com>
2018-01-23 18:59:17 +0530

    Updating PWA files

--- build/login-modals/src/pwa-service-worker/pwa-sw-DEVO_BENGALI_GR.js
+++ build/login-modals/src/pwa-service-worker/pwa-sw-DEVO_BENGALI_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?230120181852',
+		'pwa-stylesheets/css/style.css?230120181859',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-bn.css'
 	];
 
-	var STATIC_VERSION = "230120181852";
+	var STATIC_VERSION = "230120181859";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://bengali-devo-gr.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-230120181852.html";
+	var PWA_INDEX_HTML = "app-shell-230120181859.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
