688237f535bbb847203328c0a14258fe86e3afd0
techpool <suryadeep10@gmail.com>
2018-01-24 12:48:14 +0530

    Updating PWA files

--- build/social-connect/src/pwa-service-worker/pwa-sw-GAMMA_KANNADA.js
+++ build/social-connect/src/pwa-service-worker/pwa-sw-GAMMA_KANNADA.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?240120181245',
+		'pwa-stylesheets/css/style.css?240120181248',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-kn.css'
 	];
 
-	var STATIC_VERSION = "240120181245";
+	var STATIC_VERSION = "240120181248";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://kannada-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-240120181245.html";
+	var PWA_INDEX_HTML = "app-shell-240120181248.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
