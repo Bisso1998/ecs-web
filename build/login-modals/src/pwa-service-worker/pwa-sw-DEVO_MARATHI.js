aae013d3ccb9d2874fd94e061b8fc6397e0a9fe0
Libin V Babu <libinvbabu@gmail.com>
2018-01-24 16:27:27 +0530

    Updating PWA files

--- build/login-modals/src/pwa-service-worker/pwa-sw-DEVO_MARATHI.js
+++ build/login-modals/src/pwa-service-worker/pwa-sw-DEVO_MARATHI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?240120181545',
+		'pwa-stylesheets/css/style.css?240120181627',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-mr.css'
 	];
 
-	var STATIC_VERSION = "240120181545";
+	var STATIC_VERSION = "240120181627";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://marathi-devo.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-240120181545.html";
+	var PWA_INDEX_HTML = "app-shell-240120181627.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
