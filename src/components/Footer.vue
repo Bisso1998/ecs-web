47d87dac9cbd1e75b4dff3584be38d9bf0f8071e
Libin V Babu <libinvbabu@gmail.com>
2018-02-27 15:01:36 +0530

    Footer items

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,36 @@
 <template>
     <div>
         <div class="top-footer">
-            Todo: Footer columns
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="foot-title">{{ $t("footer_contact_us") }}</div>
+                        <div class="foot-items">
+                            <a :href="'tel:' + $t('footer_contact_number').replace(' ', '')"><i class="material-icons material-icons-16 vertical-middle">phone</i> {{ $t("footer_contact_number") }}</a>
+                            <a :href="'mailto:' + $t('footer_contact_email')"><i class="material-icons material-icons-16 vertical-middle">mail</i> {{ $t("footer_contact_email") }}</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        
+                    </div>
+                    <div class="col-sm-4">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
         <footer>
-            <p>
-                © {{ new Date().getFullYear() }} Nasadiya Tech. Pvt. Ltd. | 
-                <a href="/privacy-policy">{{ $t("footer_privacy_policy") }}</a> |
-                <a href="/terms-of-service">{{ $t("footer_terms_of_service") }}</a>
-            </p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>
+                            © {{ new Date().getFullYear() }} Nasadiya Tech. Pvt. Ltd. | 
+                            <a href="/privacy-policy">{{ $t("footer_privacy_policy") }}</a> |
+                            <a href="/terms-of-service">{{ $t("footer_terms_of_service") }}</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </footer>
     </div>
 </template>
@@ -27,6 +49,14 @@ export default {
     }
     footer {
         background: #191919;
-        color: #fff;
+        color: #fefefe;
+        font-size: 13px;
+        padding: 15px 0;
+        p {
+            margin: 0;
+            a {
+                color: #fefefe;
+            }
+        }
     }
 </style>
