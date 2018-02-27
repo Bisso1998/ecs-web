6f4c3c67a517b44e4f5046269e5759e904a7f27c
Suryadeep Pal <suryadeep10@gmail.com>
2018-02-27 13:09:28 +0530

    [FIX] Added fix with jQuery and Popper fixed

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -48,6 +48,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': require('../config/dev.env')
     }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      Popper: ['popper.js', 'default']
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
