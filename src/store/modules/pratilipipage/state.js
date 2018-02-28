2968636401162b8079212bee35ecc871f77d2ebe
Suryadeep Pal <suryadeep10@gmail.com>
2018-02-28 14:45:33 +0530

    [ADD] Added store for pratilipipage

---
+++ src/store/modules/pratilipipage/state.js
@@ -0,0 +1,18 @@
+export default {
+    pratilipi: {
+    	loading_state: 'LOADING',
+    	data: {}
+    },
+    userPratilipi: {
+    	loading_state: 'LOADING',
+    	data: {}
+    },
+    reviews: {
+    	loading_state: 'LOADING',
+    	data: []
+    },
+    recommendations: {
+    	loading_state: 'LOADING',
+    	data: []
+    }
+}
