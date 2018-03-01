c6a735b44baffdba24fead03190f671d755e8a22
Libin V Babu <libinvbabu@gmail.com>
2018-03-01 19:04:32 +0530

    Pratilipi page

--- src/pages/Pratilipi.vue
+++ src/pages/Pratilipi.vue
@@ -3,41 +3,52 @@
         <div class="pratilipi-page">
             <div class="container">
                 <div class="row">
-                    <div class="book-details col-md-12 col-lg-5">
-                        <div class="book-image" v-bind:style="{ backgroundImage: 'url(' + getPratilipiData.coverImageUrl  + ')' }"></div>
-                        <div class="book-title">{{ getPratilipiData.title }}</div>
-                        <router-link
-                          :to="getPratilipiData.author.pageUrl"
-                          class="author-name">
-                          <span>{{ getPratilipiData.author.name }}</span>
-                        </router-link>
-                        <div class="book-stats">
-                            <span class="avg-rating stars-green"><span class="rating-text">{{ getPratilipiData.averageRating | round(1) }}</span> <i class="material-icons">star_rate</i></span>
-                            <span class="review-count">{{ getPratilipiData.reviewCount }} {{ $t("rating_ratings") }}</span>
-                        </div>
-                        <div class="book-stats">
-                            <span class="read-count">{{ $t("pratilipi_count_reads") }}: {{ getPratilipiData.readCount }}</span>
-                            <span class="date">{{ $t("pratilipi_listing_date") }}: {{ getPratilipiData.listingDateMillis | convertDate($t) }}</span>
-                        </div>
-                        <div class="main-actions">
-                            <button class="library-btn">
-                                <span>+ {{ $t("library") }}</span>
-                            </button>
+                    <div class="book-details col-md-12 col-lg-5 p-0">
+                        <div class="card">
+                            <div class="book-image" v-bind:style="{ backgroundImage: 'url(' + getPratilipiData.coverImageUrl  + ')' }"></div>
+                            <div class="book-title">{{ getPratilipiData.title }}</div>
                             <router-link
-                              :to="getPratilipiData.readPageUrl"
-                              class="read-btn">
-                              <span>{{ $t("read") }}</span>
+                              :to="getPratilipiData.author.pageUrl"
+                              class="author-name">
+                              <span>{{ getPratilipiData.author.name }}</span>
                             </router-link>
+                            <div class="book-stats">
+                                <span class="avg-rating stars-green"><span class="rating-text">{{ getPratilipiData.averageRating | round(1) }}</span> <i class="material-icons">star_rate</i></span>
+                                <span class="review-count">{{ getPratilipiData.reviewCount }} {{ $t("rating_ratings") }}</span>
+                            </div>
+                            <div class="book-stats">
+                                <span class="read-time">{{ $t("pratilipi_reading_time") }}: {{ getPratilipiData.readingTime | showInMinutesOrHours($t) }}</span>
+                            </div>
+                            <div class="book-stats">
+                                <span class="read-count">{{ $t("pratilipi_count_reads") }}: {{ getPratilipiData.readCount }}</span>
+                                <span class="date">{{ $t("pratilipi_listing_date") }}: {{ getPratilipiData.listingDateMillis | convertDate($t) }}</span>
+                            </div>
+                            <div class="main-actions">
+                                <button class="library-btn">
+                                    <span>+ {{ $t("library") }}</span>
+                                </button>
+                                <router-link
+                                  :to="getPratilipiData.readPageUrl"
+                                  class="read-btn">
+                                  <span>{{ $t("read") }}</span>
+                                </router-link>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-12 col-lg-7">
-                        Synopsis and about Author
+                    <div class="book-synopsis col-md-12 col-lg-7 p-0">
+                        <div class="card">
+                            Synopsis and about Author
+                        </div>
                     </div>
-                    <div class="col-md-12">
-                        Reviews
+                    <div class="book-reviews col-md-12 p-0">
+                        <div class="card">
+                            Reviews
+                        </div>
                     </div>
-                    <div class="col-md-12">
-                        Recomendations
+                    <div class="book-recomendations col-md-12 p-0">
+                        <div class="card">
+                            Recomendations
+                        </div>
                     </div>
                 </div>
             </div>
@@ -97,11 +108,10 @@ export default {
         @media screen and (max-width: 992px ) {
             margin-top: 140px;
         }
-        .book-details {
-            background: #fff;
-            border: 1px solid #e9e9e9;
-            position: relative;
+        .card {
             margin: 10px;
+        }
+        .book-details {
             .book-image {
                 background-size: contain;
                 background-repeat: no-repeat;
@@ -141,6 +151,15 @@ export default {
                         padding-left: 5px;
                         vertical-align: middle;
                     }
+                    &.stars-green {
+                        background: #37be5f;
+                    }
+                    &.stars-orange {
+                        background: #ffb500;
+                    }
+                    &.stars-red {
+                        background: #ff6d13;
+                    }
                 }
                 .review-count {
                     font-size: 12px;
@@ -153,12 +172,44 @@ export default {
                     border-right: 1px solid;
                     padding-right: 5px;
                     margin: 0;
-                    line-height: 10px;
+                    line-height: 12px;
+                    display: inline-block;
+                }
+                .date {
+                    display: inline-block;
+                    padding-left: 5px;
+                    display: inline-block;
                 }
             }
             .main-actions {
                 width: 100%;
                 background: #fff;
+                .library-btn, .read-btn {
+                    display: inline-block;
+                    width: 48%;
+                    height: 52px;
+                    line-height: 52px;
+                    font-size: 17px;
+                    text-align: center;
+                    background: #fff;
+                    color: #fff;
+                    font-weight: 700;
+                    border: 0;
+                    outline: none;
+                    padding: 0;
+                    margin: 10px 0;
+                    cursor: pointer;
+                    &:hover {
+                        text-decoration: none;
+                        opacity: 0.8;
+                    }
+                }
+                .library-btn {
+                    background: #9E9E9E;
+                }
+                .read-btn {
+                    background: #d0021b;
+                }
             }
         }
     }
