920343fd9827fc81187ee6bd2f89b64cde1c171d
Libin V Babu <libinvbabu@gmail.com>
2018-03-02 12:58:02 +0530

    Updating author section

--- src/pages/Pratilipi.vue
+++ src/pages/Pratilipi.vue
@@ -37,12 +37,30 @@
                     </div>
                     <div class="book-synopsis col-md-12 col-lg-7 p-0">
                         <div class="card">
-                            Synopsis and about Author
+                            <div class="head-title">{{ $t("pratilipi_summary") }}</div>
+                            <p class="show-more-height">{{ getPratilipiData.summary }}</p>
+                            <div class="show-more-button">
+                                <button class="show_more">{{ $t("show_more") }}</button>
+                            </div>
+                            
+                            <div class="head-title">{{ $t("author_about") }}</div>
+                            <router-link
+                                :to="getPratilipiData.author.pageUrl"
+                                class="author-link">
+                                <img :src="getPratilipiData.author.profileImageUrl" alt="author" class="auth-img" >
+                                <div class="auth-name">{{ getPratilipiData.author.displayName }}</div>
+                            </router-link>
+                            <button class="btn btn-light follow-link"><i class="material-icons">person_add</i> {{ $t("author_follow") }}</button>
+                            <button class="btn btn-light follow-link following" style="display: none;">{{ $t("author_unfollow") }}</button>
+                            <p class="auth-desc show-more-height">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="show-more-button">
+                                <button class="show_more">{{ $t("show_more") }}</button>
+                            </div>
                         </div>
                     </div>
                     <div class="book-reviews col-md-12 p-0">
                         <div class="card">
-                            Reviews
+                            <div class="head-title">{{ $t("pratilipi_count_reviews") }}</div>
                         </div>
                     </div>
                     <div class="book-recomendations col-md-12 p-0">
@@ -113,8 +131,76 @@ export default {
         }
         .card {
             margin: 10px;
+            display: block;
+            text-align: left;
+            border-radius: 0;
+            .head-title {
+                text-align: left;
+                font-weight: bold;
+                font-size: 18px;
+                border-left: 3px solid #d0021b;
+                padding-left: 10px;
+                margin: 10px 0;
+            }
+            p {
+                text-align: left;
+                margin: 10px;
+                font-size: 14px;
+            }
+            .author-link {
+                color: #d0021b;
+                .auth-img {
+                    border-radius: 50%;
+                    width: 50px;
+                    height: 50px;
+                    margin-left: 10px;
+                    display: inline-block;
+                }
+                .auth-name {
+                    text-align: left;
+                    display: inline-block;
+                    margin: 0 10px;
+                    font-size: 14px;
+                    max-width: 110px;
+                    vertical-align: middle;
+                }
+                &:hover {
+                    text-decoration: none;
+                }
+            }
+            .follow-link {
+                color: #d0021b;
+                float: right;
+                margin: 4px 10px 0 0;
+                font-size: 14px;
+                i {
+                    vertical-align: middle;
+                    padding-right: 5px;
+                    font-size: 18px;
+                }
+            }
+            .auth-desc {
+                text-align: left;
+                margin-left: 10px;
+            }
+            .show-more-button {
+                text-align: right;
+                .show_more {
+                    color: #d0021b;
+                    background: none;
+                    border: 0;
+                    outline: none;
+                    text-align: right;
+                    font-size: 14px;
+                    margin: 0 10px 10px;
+                    cursor: pointer;
+                }
+            }
         }
         .book-details {
+            .card {
+                text-align: center;
+            }
             .book-image {
                 background-size: contain;
                 background-repeat: no-repeat;
