6e418368e4b3c696a79fe3f024480c8c2f969cf5
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-05 17:43:43 +0530

    [ADD] Added about us page

---
+++ build/about/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    gu: require('./about-us-gu.json'),
+    hi: require('./about-us-hi.json'),
+    bn: require('./about-us-bn.json'),
+    kn: require('./about-us-kn.json'),
+    ml: require('./about-us-ml.json'),
+    mr: require('./about-us-mr.json'),
+    ta: require('./about-us-ta.json'),
+    te: require('./about-us-te.json')
+}
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,9 +4,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h2>About Page Title</h2>
+                        <h2>__ABOUT_US_TITLE__</h2>
                         <div class="page-content">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            __ABOUT_US_BODY__
                         </div>
                     </div>
                 </div>
