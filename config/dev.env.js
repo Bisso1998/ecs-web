173e4c21a3283b3ea566ec219f4fd9be54c14a17
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-05 09:51:37 +0530

    [ADD] Added language as an environment variable

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,5 +8,6 @@ module.exports = merge(prodEnv, {
   FIREBASE_AUTH_DOMAIN: '"devo-pratilipi.firebaseapp.com"',
   FIREBASE_DATABASE_URL: '"https://devo-pratilipi.firebaseio.com"',
   FIREBASE_PROJECT_ID: '"devo-pratilipi"',
-  FIREBASE_STORAGE_BUCKET: '"devo-pratilipi.appspot.com"'
+  FIREBASE_STORAGE_BUCKET: '"devo-pratilipi.appspot.com"',
+  LANGUAGE: `"${process.env.LANGUAGE || 'hi'}"`
 })
