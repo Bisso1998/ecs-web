16ee7a019e5d44f32b3e9e8213d45c2b6363e9ab
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-05 14:28:45 +0530

    [ADD] Added events listing page

---
+++ src/pages/Event.vue
@@ -0,0 +1,65 @@
+<template>
+    <MainLayout>
+        <div class="static-page">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Privacy Page Title</h2>
+                        <div class="page-content">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script>
+import MainLayout from '@/layout/main-layout.vue';
+import constants from '@/constants'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    components: {
+        MainLayout
+    },
+    computed: {
+        ...mapGetters('eventspage', [
+            'getEventsLoadingState',
+            'getEventsData'
+        ])
+    },
+    methods: {
+        ...mapActions('eventspage', [
+            'fetchListOfEvents'
+        ]),
+    },
+    created() {
+        const currentLocale = process.env.LANGUAGE;
+        constants.LANGUAGES.forEach((eachLanguage) => {
+            if (eachLanguage.shortName === currentLocale) {
+                this.fetchListOfEvents(eachLanguage.fullName.toUpperCase())
+            }
+        });
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.static-page {
+    margin-top: 85px;
+    text-align: left;
+    @media screen and (max-width: 992px ) {
+        margin-top: 140px;
+    }
+    h2 {
+        font-size: 24px;
+        font-weight: bold;
+        text-align: left;
+        border-left: 3px solid #d0021b;
+        padding-left: 10px;
+        margin: 10px 0;
+    }
+}
+</style>
