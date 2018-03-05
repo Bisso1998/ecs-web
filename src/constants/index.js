591a0c1c8898b9ee7b3d2d148d12459d4cc3dabb
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-05 09:52:33 +0530

    [REMOVE] Removed i18n and navigation files

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,3 @@
-import bengaliCategory from './categories/navigation-bn'
-import gujaratiCategory from './categories/navigation-gu'
-import hindiCategory from './categories/navigation-hi'
-import kannadaCategory from './categories/navigation-kn'
-import malayalamCategory from './categories/navigation-ml'
-import marathiCategory from './categories/navigation-mr'
-import tamilCategory from './categories/navigation-ta'
-import teluguCategory from './categories/navigation-te'
-
 export default {
     LANGUAGES: [
         {
@@ -42,14 +33,5 @@ export default {
             fullName: 'telugu'
         },
     ],
-    CATEGORY_DATA: {
-        bn: bengaliCategory,
-        gu: gujaratiCategory,
-        hi: hindiCategory,
-        kn: kannadaCategory,
-        ml: malayalamCategory,
-        mr: marathiCategory,
-        ta: tamilCategory,
-        te: teluguCategory,
-    }
+    CATEGORY_DATA: JSON.parse('__NAVIGATION_SECTION_LIST__')
 }
