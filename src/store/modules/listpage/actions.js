15e6e31727e3d6c1f414901afb95b3ad909ff77b
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-06 14:43:51 +0530

    [ADD] Added APIs for drafted contents in writer page

--- src/store/modules/listpage/actions.js
+++ src/store/modules/listpage/actions.js
@@ -4,7 +4,6 @@ export default {
 
     fetchInitialListPagePratilipis({ commit, state }, { listName, language, resultCount }) {
         commit('setListPageInitialDataLoadingTrue');
-        console.log(language);
         DataAccessor.getPratilipiListByListName(listName, null, null, resultCount, language, function(data) {
             if (data.status === 200) {
                 commit('setListPageInitialDataLoadingSuccess', data.response);
@@ -16,7 +15,6 @@ export default {
 
     fetchMorePratilipisForListPage({ commit, state }, { listName, language, resultCount }) {
         commit('setListPageDynamicLoadingTrue');
-        console.log(language);
         DataAccessor.getPratilipiListByListName(listName, state.cursor, null, resultCount, language, function(data) {
             if (data.status === 200) {
                 commit('setListPageDynamicLoadingSuccess', data.response);
