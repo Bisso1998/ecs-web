e8fd9b27a41e6692edd212b6b343e0b52759def1
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-13 14:35:43 +0530

    [FIX] Added user details loading on the main app loading

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,60 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-    name: 'App'
+    name: 'App',
+    methods: {
+        ...mapActions([
+            'fetchUserDetails',
+            'fetchInitialNotifications',
+            'setNotificationCount'
+        ])
+    },
+    computed: {
+        ...mapGetters([
+            'getUserDetails'
+        ])
+    },
+    watch: {
+        'getUserDetails.isGuest'(isGuest) {
+            if (!isGuest) {
+                import('firebase').then((firebase) => {
+                    var config = {
+                        apiKey: process.env.FIREBASE_API_KEY,
+                        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+                        databaseURL: process.env.FIREBASE_DATABASE_URL,
+                        storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+                    };
+                    const firebaseApp = firebase.initializeApp(config);
+                    const db = firebaseApp.database();
+                    const that = this;
+
+                    firebase.auth().onAuthStateChanged( function( fbUser ) {
+                        if( fbUser ) {
+                            var newNotificationCountNode = firebase.database().ref( "NOTIFICATION" ).child( fbUser.uid ).child( "newNotificationCount" );
+                            newNotificationCountNode.on( 'value', function( snapshot ) {
+                                var newNotificationCount = snapshot.val() != null ? snapshot.val() : 0;
+                                that.setNotificationCount(newNotificationCount);
+                            });
+                            var userPreferencesNode = firebase.database().ref( "PREFERENCE" ).child( fbUser.uid );
+                            userPreferencesNode.on( 'value', function( snapshot ) {
+                                var userPreferences = snapshot.val() != null ? snapshot.val() : {};
+                                
+                                console.log(userPreferences);
+                            });
+                        } else {
+                            firebase.auth().signInWithCustomToken( that.getUserDetails.firebaseToken );
+                        }
+                    });
+                });
+            }
+        }
+    },
+    created() {
+        this.fetchUserDetails();
+    }
 }
 </script>
 
