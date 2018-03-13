abf5f4ea31298bcb57bc457423fe86137a388c90
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-13 12:30:22 +0530

    [ADD] Added pratilipi image upload

--- src/store/modules/pratilipipage/state.js
+++ src/store/modules/pratilipipage/state.js
@@ -1,7 +1,8 @@
 export default {
     pratilipi: {
     	loading_state: 'LOADING',
-    	data: {}
+    	data: {},
+    	pratilipi_image_upload_state: 'LOADING_SUCCESS'
     },
     userPratilipi: {
     	loading_state: 'LOADING',
