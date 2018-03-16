65cc4c2b8f39a3afe1c3a022dd8f97a1d29c2857
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-16 11:54:10 +0530

    [FIX] Fixed for serving static files

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const cookie = require('cookie');
+const request = require('request');
 const path = require('path');
 const serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist/hi/"));
+
+app.use((req, res, next) => {
+    const access_token = cookie.parse(req.headers.cookie || '').access_token;
+    if (req.path.endsWith('css') || req.path.endsWith('map') || req.path.endsWith('js')) {
+    	console.log('Gotcha! No access token for you! - ', req.path);
+    	next();
+    	return;
+    }
+    request.get({
+        url: 'https://gamma.pratilipi.com/user/accesstoken?accessToken=' + access_token,
+        json: true
+    }, function(errorInResponse, response, data) {
+        res.setHeader('set-cookie', 'access_token=' + data.accessToken);
+        res.cookie('access_token', data.accessToken, {
+			domain: process.env.DOMAIN || 'localhost',
+			path: '/',
+			httpOnly: false,
+			maxAge: 60 * 60 * 1000,
+	    });
+        next();
+    });
+
+})
+
+app.use(serveStatic(__dirname + `/dist/${process.env.LANGUAGE}/`));
+
+app.use((req, res) => {
+	res.sendFile(__dirname + `/dist/${process.env.LANGUAGE}/index.html`)
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started ' + port);
