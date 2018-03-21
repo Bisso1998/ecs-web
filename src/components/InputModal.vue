c391fcc996934f709280bc914eaf3cc8ac1056ef
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-21 16:47:36 +0530

    [ADD] Changed input modals to a seperate component

---
+++ src/components/InputModal.vue
@@ -0,0 +1,78 @@
+<template>
+    <!-- Report Modal -->
+    <div class="modal fade reportModal" id="reportModal" tabindex="-1" role="dialog" aria-labelledby="reportModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="reportModalLabel">{{ getInputModalTitle }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="material-icons">close</i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="reportModalTextarea">{{ getInputModalTitle }}</label>
+                            <textarea :value="getInputModalActionAndData.prefilled_value" @input="updatePrefilledValue($event.target.value)" class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="button" class="btn btn-primary btn-submit" @click="dispatchActionAndCloseModal">__("submit")</button>
+                        <button type="button" class="cancel" data-dismiss="modal" aria-label="Close">__("cancel")</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        ...mapGetters([
+            'getInputModalTitle',
+            'getInputModalActionAndData'
+        ])
+    },
+    methods: {
+        ...mapActions([
+            'dispatchAction',
+            'updatePrefilledValue'
+        ]),
+        dispatchActionAndCloseModal() {
+            $('#reportModal').modal('hide');
+            this.dispatchAction();
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.reportModal {
+    text-align: left;
+    max-width: 350px;
+    margin: 50px auto;
+    .form-group {
+        font-size: 14px;
+    }
+    .btn-submit {
+        background: #d0021b;
+        border: 0;
+        font-size: 14px;
+        float: right;
+    }
+    .cancel {
+        background: none;
+        border: 0;
+        float: right;
+        font-size: 12px;
+        line-height: 33px;
+    }
+}
+</style>
