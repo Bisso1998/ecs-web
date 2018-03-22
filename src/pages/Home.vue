8e5d6096c27d086e23a6121c8b35fa725f31e265
Libin V Babu <libinvbabu@gmail.com>
2018-03-22 15:50:44 +0530

    Minimising footer on mobile

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -69,6 +69,7 @@ export default {
 <style  lang="scss" scoped>
     .home-page {
         margin-top: 85px;
+        min-height: 700px;
         @media screen and (max-width: 992px ) {
             margin-top: 65px;
         }
