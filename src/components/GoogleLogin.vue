dc367e6ea2a9d29dae78149cbe4249a8e6edb09a
Suryadeep Pal <suryadeep10@gmail.com>
2018-03-23 17:19:32 +0530

    [ADD] Added environment variabels and building logic

---
+++ src/components/GoogleLogin.vue
@@ -0,0 +1,85 @@
+<template>
+    <button @click="loginToGoogle" type="button" name="button" class="google"><icon name="google"></icon>__("user_sign_in_with_google")</button>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import mixins from '@/mixins';
+
+export default {
+    name: 'Google-Login-Button',
+    mixins: [
+        mixins
+    ],
+    methods: {
+        ...mapActions([
+            'loginUserWithGoogleToken'
+        ]),
+        loginToGoogle() {
+            console.log('hi')
+            const GoogleAuth = gapi.auth2.getAuthInstance();
+            console.log(GoogleAuth);
+            GoogleAuth.signIn().then( function( googleUser ) {
+                this.loginUserWithGoogleToken(googleUser.getAuthResponse().id_token);
+            }, function( error ) {
+                console.log( JSON.stringify( error, undefined, 2 ) );
+            });
+        }
+    },
+    components: {
+        
+    },
+    created() {
+
+        const googleClientId = process.env.GOOGLE_CLIENT_ID;
+        console.log();
+        if (document.getElementById('google-client-jssdk')) {
+            return;
+        }
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//apis.google.com/js/api.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'google-client-jssdk'));
+        document.getElementById('google-client-jssdk').onload = () => {
+            gapi.load( 'auth2', function() {
+                gapi.auth2.init({
+                    client_id: `${googleClientId}`,
+                    cookiepolicy: 'single_host_origin'
+                });
+            });
+        }
+    }
+}
+</script>
+
+<style  lang="scss" scoped>
+button.fb, button.google {
+    background: #3b5998;
+    color: #fff;
+    display: block;
+    width: 100%;
+    height: 45px;
+    max-width: 400px;
+    margin: 10px auto;
+    border-radius: 3px;
+    border: 0;
+    outline: none;
+    text-align: left;
+    font-size: 14px;
+    cursor: pointer;
+    &:hover {
+        opacity: 0.9;
+    }
+    .fa-icon {
+        vertical-align: middle;
+        margin: 0 15px;
+    }
+}
+button.google {
+    background: #DD4B39;
+}
+</style>
---
+++ src/static_scripts/facebook_analytics.js
@@ -0,0 +1,18 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: process.env.FACEBOOK_APP_ID,
+        cookie: true,
+        xfbml: true,
+        autoLogAppEvents: false,
+        version: 'v2.10'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
