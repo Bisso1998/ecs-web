7d8c7231d2e4c60211088275d2d02739cd25cf20
Libin V Babu <libinvbabu@gmail.com>
2018-04-25 14:41:12 +0530

    Adding rating v2

--- src/static_scripts/experiment_events/reader_v2.js
+++ src/static_scripts/experiment_events/reader_v2.js
@@ -1,3 +1,47 @@
 export default {
-    
+    "GOHOME_HEADER_GLOBAL": {
+        "LOCATION": "HEADER",
+        "ACTION": "GOHOME"
+    },
+    "GOLOGIN_HEADER_GLOBAL": {
+        "LOCATION": "HEADER",
+        "ACTION": "GOLOGIN"
+    },
+    "GOREGISTER_HEADER_GLOBAL": {
+        "LOCATION": "HEADER",
+        "ACTION": "GOMYPROFILE"
+    },
+    "GOCREATE_HEADER_GLOBAL": {
+        "LOCATION": "HEADER",
+        "ACTION": "GOMYPROFILE"
+    },
+    "GODISCOVER_HEADER_GLOBAL": {
+        "LOCATION": "HEADER",
+        "ACTION": "GOMYPROFILE"
+    },
+    "LANDED_READERM_READER": {
+        "SCREEN_NAME": "READER",
+        "LOCATION": "READERM",
+        "ACTION": "LANDED"
+    },
+    "RATE_BOOKEND_READER": {
+        "SCREEN_NAME": "READER",
+        "LOCATION": "BOOKEND",
+        "ACTION": "RATE"
+    },
+    "EDITRATE_BOOKEND_READER": {
+        "SCREEN_NAME": "READER",
+        "LOCATION": "BOOKEND",
+        "ACTION": "EDITRATE"
+    },
+    "REVIEW_BOOKEND_READER": {
+        "SCREEN_NAME": "READER",
+        "LOCATION": "BOOKEND",
+        "ACTION": "REVIEW"
+    },
+    "EDITREVIEW_BOOKEND_READER": {
+        "SCREEN_NAME": "READER",
+        "LOCATION": "BOOKEND",
+        "ACTION": "EDITREVIEW"
+    },
 }
