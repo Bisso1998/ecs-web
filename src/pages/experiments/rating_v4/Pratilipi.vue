8b8e52b46cef772afd841a899d9b6d420cda9480
Libin V Babu <libinvbabu@gmail.com>
2018-04-26 15:58:46 +0530

    Adding bookpage follow v4

--- src/pages/experiments/rating_v4/Pratilipi.vue
+++ src/pages/experiments/rating_v4/Pratilipi.vue
@@ -167,7 +167,7 @@
 <script>
 import MainLayout from '@/layout/experiments/rating_v4/main-layout.vue';
 import Recommendation from '@/components/Recommendation.vue';
-import AboutAuthor from '@/components/AboutAuthor.vue';
+import AboutAuthor from '@/components/experiments/rating_v4/AboutAuthor.vue';
 import Spinner from '@/components/Spinner.vue';
 import Reviews from '@/components/experiments/rating_v4/Reviews.vue';
 import ServerError from '@/components/ServerError.vue';
