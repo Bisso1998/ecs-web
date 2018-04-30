a2c0cf18ce71ace313dd021f091f13f9110098b2
Libin V Babu <libinvbabu@gmail.com>
2018-04-30 16:40:55 +0530

    Adding experiment id to v3

--- src/components/experiments/home_v3/PratilipiList.vue
+++ src/components/experiments/home_v3/PratilipiList.vue
@@ -103,7 +103,7 @@ export default {
         },
         handleBeforeChange() {
             if (this.$route.meta.store === 'homepage') {
-                this.triggerAnanlyticsEvent(`SWIPE_COLLECTIONS_HOME`, 'CONTROL', {
+                this.triggerAnanlyticsEvent(`SWIPE_COLLECTIONS_HOME`, 'WGEN019', {
                     'USER_ID': this.getUserDetails.userId,
                     'PARENT_ID': this.listPageUrl
                 });
@@ -115,7 +115,7 @@ export default {
             this.$refs.slick.reSlick();
         },
         triggerListLink() {
-            this.triggerAnanlyticsEvent(`CLICKCOLLECTION_${this.screenLocation}_${this.screenName}`, 'CONTROL', {
+            this.triggerAnanlyticsEvent(`CLICKCOLLECTION_${this.screenLocation}_${this.screenName}`, 'WGEN019', {
                 'USER_ID': this.getUserDetails.userId,
                 'PARENT_ID': this.listPageUrl
             });
@@ -135,7 +135,7 @@ export default {
         'inViewport.now': function(visible) {
             if (visible) {
                 if (this.screenName === 'HOME') {
-                    this.triggerAnanlyticsEvent(`VIEWED_COLLECTIONS_HOME`, 'CONTROL', {
+                    this.triggerAnanlyticsEvent(`VIEWED_COLLECTIONS_HOME`, 'WGEN019', {
                         'USER_ID': this.getUserDetails.userId,
                         'PARENT_ID': this.listPageUrl
                     });
