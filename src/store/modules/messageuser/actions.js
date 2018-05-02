f577f4c2b4bc684d765c6504c8fa51f613c4549a
Roshan Alexander <roshanalexander88@gmail.com>
2018-05-02 15:36:30 +0530

    Added store for message user

---
+++ src/store/modules/messageuser/actions.js
@@ -0,0 +1,14 @@
+import DataAccessor from '@/utils/DataAccessor'
+import $ from 'jquery'
+
+export default {
+
+    addMessageTargetUserDetails({ commit, state }, {}) {
+
+    },
+
+    resetMessageTargetUserDetails({ commit, state }, {}) {
+
+    }
+
+}
