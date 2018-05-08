dc5336eb31ef9a24043332d88b4d7061f3cfb2e5
Suryadeep Pal <suryadeep10@gmail.com>
2018-05-08 11:37:14 +0530

    [ADD] Added Facebook SDK version to config

--- config/dev.env.js
+++ config/dev.env.js
@@ -12,6 +12,7 @@ module.exports = merge(prodEnv, {
   FIREBASE_STORAGE_BUCKET: '"prod-pratilipi.appspot.com"',
   LANGUAGE: `"${process.env.LANGUAGE || 'hi'}"`,
   FACEBOOK_APP_ID: '368844269953501',
+  FACEBOOK_SDK_VERSION: "'2.10v'",
   AMPLITUDE_APP_ID: "'73ff7c6e07c484e37d0a9d7d240312c6'",
   GA_WEBSITE: "'PWA'",
   GA_WEBSITE_MODE: "'Standard'",
