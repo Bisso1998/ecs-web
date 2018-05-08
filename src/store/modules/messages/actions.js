11a82bc7b8e1016d14aa91c8d2b82640991afe48
Suryadeep Pal <suryadeep10@gmail.com>
2018-05-08 11:39:42 +0530

    [FIX] Removed unneeded dependencies

--- src/store/modules/messages/actions.js
+++ src/store/modules/messages/actions.js
@@ -1,10 +1,6 @@
-import DataAccessor from '@/utils/DataAccessor'
-import $ from 'jquery'
-
 export default {
 
     saveConversationsDataToCache({ commit }, { conversations, channelLastMessage, channelLastReadMessage, fetchedChannelMetadataData }) {
-        debugger;
         commit('setConversationListCached', conversations);
         commit('setChannelLastMessageCached', channelLastMessage);
         commit('setChannelLastReadMessageCached', channelLastReadMessage);
