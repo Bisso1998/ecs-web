e66520189e588c69843a1a290f7ecff3c74909a1
Libin V Babu <libinvbabu@gmail.com>
2018-06-11 18:12:03 +0530

    Updating about author modal title

--- src/components/AboutAuthor.vue
+++ src/components/AboutAuthor.vue
@@ -23,7 +23,7 @@
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title">__("pratilipi_summary")</h5>
+                    <h5 class="modal-title">__("author_about")</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
